76fe50bdf656b014abd71771cc80b183748a5df9
Tobias Lorenz <tobias.lorenz@bitmazk.com>
2014-04-14 16:01:42 +0200

    show all relevant fields in admin list

--- tinylinks/admin.py
+++ tinylinks/admin.py
@@ -1,15 +1,21 @@
 """Admin sites for the ``django-tinylinks`` app."""
 from django.contrib import admin
+from django.template.defaultfilters import truncatechars
+from django.utils.translation import ugettext_lazy as _
 
 from tinylinks.forms import TinylinkAdminForm
 from tinylinks.models import Tinylink
 
 
 class TinylinkAdmin(admin.ModelAdmin):
-    list_display = ('short_url', 'long_url', 'amount_of_views', 'user',
-                    'last_checked')
+    list_display = ('short_url', 'url_truncated', 'amount_of_views', 'user',
+                    'last_checked', 'is_broken', 'validation_error')
     search_fields = ['short_url', 'long_url']
     form = TinylinkAdminForm
+
+    def url_truncated(self, obj):
+        return truncatechars(obj.long_url, 60)
+    url_truncated.short_description = _('Long URL')
 
 
 admin.site.register(Tinylink, TinylinkAdmin)
