b98ade873824a5fa9de6f3dfdadcbb579994f565
Tobias Lorenz <tobias.lorenz@bitmazk.com>
2014-04-14 16:23:54 +0200

    fixed irritating status field in admin

--- tinylinks/admin.py
+++ tinylinks/admin.py
@@ -9,13 +9,19 @@ from tinylinks.models import Tinylink
 
 class TinylinkAdmin(admin.ModelAdmin):
     list_display = ('short_url', 'url_truncated', 'amount_of_views', 'user',
-                    'last_checked', 'is_broken', 'validation_error')
+                    'last_checked', 'status', 'validation_error')
     search_fields = ['short_url', 'long_url']
     form = TinylinkAdminForm
 
     def url_truncated(self, obj):
         return truncatechars(obj.long_url, 60)
     url_truncated.short_description = _('Long URL')
+
+    def status(self, obj):
+        if not obj.is_broken:
+            return _('OK')
+        return _('Link broken')
+    status.short_description = _('Status')
 
 
 admin.site.register(Tinylink, TinylinkAdmin)
